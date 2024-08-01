ea96ee4775206446ed435698fca1e81227c70d59
Yeuris <ylopezjaime@gmail.com>
2024-08-01 19:45:44 -0400

    modified:   ubuntu_script_.sh

--- ubuntu_script_.sh
+++ ubuntu_script_.sh
@@ -83,3 +83,15 @@ sudo nala install ripgrep
 # Installing Neovim
 
 brew install neovim
+
+# Installing Github CLI
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+
